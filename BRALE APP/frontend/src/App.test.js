7b0f4932dbebb9105757672d75b88b2db35b1683
Brandon Ashby <69142310+Braash@users.noreply.github.com>
2021-11-16 11:16:04 +0200

    Delete BRALE APP directory

--- BRALE APP/frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'; 
-import App from './App';
-import renderer from 'react-test-renderer'// Import test renderer
-
-test('Renders without crashing', () => {
-  const tree = renderer.create(<App />).toJSON(); 
-  expect(tree).toMatchSnapshot();  //Expect the JSON DOM tree to match snapshot.
-});
